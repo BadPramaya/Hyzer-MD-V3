b95a4f58151c3d47972db4f1dbe2af8516a64265
x_pramaya_x <78118894+BadPramaya@users.noreply.github.com>
2022-07-21 20:49:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 //=============『 Utama 』================== //
-global.owner = ['6287892711054'] 
+global.owner = ['94770611075'] 
 global.mods = [] 
 global.prems = [] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'Hyzer Official'
-global.numberowner = '6287892711054'
-global.instagram = 'https://instagram.com/hyzer_stfu'
-global.github = 'https://github.com/Hyzerr'
-global.dana = '087892711054'
-global.pulsa = '085212023766'
-global.gopay = '087892711054'
+global.nameowner = 'ʜᴇʟʟ ʙᴏʏ ©️x_ᴘʀᴀᴍᴀʏᴀ_x'
+global.numberowner = '94770611075'
+global.instagram = 'https://chat.whatsapp.com/Go9cBxSneUpGDbl6gKdF7E'
+global.github = 'https://chat.whatsapp.com/Go9cBxSneUpGDbl6gKdF7E'
+global.dana = '94770611075'
+global.pulsa = '94770611075'
+global.gopay = '94770611075'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'Yuno Gasai Bot'
-global.gc = 'bit.ly/AnyaBot-Group'
-global.web = 'https://github.com/Hyzerr' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.namebot = 'ʜᴇʟʟ ʙᴏʏ ©️x_ᴘʀᴀᴍᴀʏᴀ_x'
+global.gc = 'https://chat.whatsapp.com/Go9cBxSneUpGDbl6gKdF7E'
+global.web = 'https://chat.whatsapp.com/Go9cBxSneUpGDbl6gKdF7E' //change it to your website, you can link ig, link github, link yt, if the link is gc it will look different again. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
@@ -25,8 +25,8 @@ global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
 global.wm = 'ꜱʜɪʀᴏ ɪᴋᴀꜱᴀᴋɪ ʙᴏᴛ'
 global.watermark = wm
-global.wm2 = '                     「 𝚂𝙷𝙸𝚁𝙾-𝙼𝙳 あ⁩ 」'
-global.wm3 = '𝚂𝙷𝙸𝚁𝙾-𝙼𝙳'
+global.wm2 = '                     「 ʜᴇʟʟ ʙᴏʏ ©️x_ᴘʀᴀᴍᴀʏᴀ_x あ⁩ 」'
+global.wm3 = 'ʜᴇʟʟ ʙᴏʏ ©️x_ᴘʀᴀᴍᴀʏᴀ_x'
 global.htki = '––––––『' 
 global.htka = '』––––––'
 global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
@@ -37,7 +37,7 @@ global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = '2022'
-global.author = '© Hyzer Official'
+global.author = '© ʜᴇʟʟ ʙᴏʏ ©️x_ᴘʀᴀᴍᴀʏᴀ_x'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
